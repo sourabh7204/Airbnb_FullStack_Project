cc4d0a8bc9d778761348d5a270195b5c71eb786d
sourabh7204 <sourabh722004@gmail.com>
2025-04-12 22:32:58 +0530

    Added Boilerplate

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const listing = require("./models/listing.js");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/WanderLust";
 
@@ -22,9 +23,11 @@ async function main() {
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
 
 app.get("/", (req, res) => {
   res.send("Hi, I am root");
@@ -63,9 +66,17 @@ app.get("/listings/:id/edit", async (req, res) => {
 });
 
 //Update Route
-app.use("/listings/:id", async (req, res) => {
+app.put("/listings/:id", async (req, res) => {
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect("/listings");
+});
+
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndDelete(id);
+  console.log(deleteListing);
   res.redirect("/listings");
 });
 
