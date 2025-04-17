4a93dfcc7103c2ff168efb09b429e1aba508b475
sourabh7204 <sourabh722004@gmail.com>
2025-04-17 22:01:57 +0530

    Added Form Validations

---
+++ public/js/script.js
@@ -0,0 +1,23 @@
+(() => {
+  "use strict";
+
+  // Wait for the DOM to be fully loaded
+  document.addEventListener("DOMContentLoaded", () => {
+    const forms = document.querySelectorAll(".needs-validation");
+
+    Array.from(forms).forEach((form) => {
+      form.addEventListener(
+        "submit",
+        (event) => {
+          if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+
+          form.classList.add("was-validated");
+        },
+        false
+      );
+    });
+  });
+})();
