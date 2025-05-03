f4ce8b72fd265c059a9675da87fab69f887c62a6
sourabh7204 <sourabh722004@gmail.com>
2025-05-03 22:17:10 +0530

    used npm

--- classroom/routes/server.js
+++ classroom/routes/server.js
@@ -1,8 +1,36 @@
 const express = require("express");
 const app = express();
-const users = require("./routes/user.js");
-const posts = require("./routes/post.js");
+// const users = require("../routes/user.js");
+// const posts = require("../routes/post.js");
 const session = require("express-session");
+
+const sessionOptions = {
+  secret: "mysupersecratestring",
+  resave: false,
+  saveUninitialized: true,
+};
+
+app.use(session(sessionOptions));
+
+app.get("/register", (req, res) => {
+  let { name = "anonymous" } = req.query;
+  res.send(name);
+});
+
+app.get("/hello");
+
+// app.get("/reqcount", (req, res) => {
+//   if (req.session.count) {
+//     req.session.count++;
+//   } else {
+//     req.session.count = 1;
+//   }
+//   res.send(`You sent a request ${req.session.count} times.`);
+// });
+
+// app.get("/test", (req, res) => {
+//   res.send("test successful!");
+// });
 
 app.listen(3000, () => {
   console.log("server is listening to 3000");
