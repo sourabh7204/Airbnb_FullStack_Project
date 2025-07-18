fd64cde15c2d8ad90a7f15b314bedd032b45eb85
sourabh7204 <sourabh722004@gmail.com>
2025-07-18 23:39:45 +0530

    Added Signup Method

--- app.js
+++ app.js
@@ -14,8 +14,9 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingsRouter = require("./routes/listing.js");
+const reviewsRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 // Database connection
 const MONGO_URL = "mongodb://127.0.0.1:27017/WanderLust";
@@ -87,8 +88,9 @@ app.get("/demouser", async (req, res, next) => {
   }
 });
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+app.use("/listings", listingsRouter);
+app.use("/listings/:id/reviews", reviewsRouter);
+app.use("/", userRouter);
 
 // Catch-all for invalid routes
 app.all(/.*/, (req, res, next) => {
