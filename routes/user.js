751f9fd51b3bb9147a3e169fa4c738549675a64b
sourabh7204 <sourabh722004@gmail.com>
2025-07-19 22:06:12 +0530

    Added Signup

--- routes/user.js
+++ routes/user.js
@@ -5,4 +5,13 @@ router.get("/signup", (req, res) => {
   res.render("users/signup.ejs");
 });
 
+router.post("/signup", async (req, res) => {
+  let { username, email, password } = req.body;
+  const newUser = new UserActivation({ email, username });
+  const registerUser = await User.register(newUser, password);
+  console.log(registerUser);
+  req.flash("success", "Welcome to Wanderlust!");
+  res.redirect("/listings");
+});
+
 module.exports = router;
