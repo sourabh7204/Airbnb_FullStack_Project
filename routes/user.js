63c95b7ebab877cecfe1676ac166b9af2231b6a1
sourabh7204 <sourabh722004@gmail.com>
2025-07-20 23:11:26 +0530

    Added Logout Option

--- routes/user.js
+++ routes/user.js
@@ -47,4 +47,14 @@ router.post(
   }
 );
 
+router.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    req.flash("success", "you are logged out!");
+    res.redirect("/listings");
+  });
+});
+
 module.exports = router;
