3df994cc314421e55d23cc0aabf84435bcb2c385
sourabh7204 <sourabh722004@gmail.com>
2025-07-24 23:04:55 +0530

    Cloud Storage Setup

--- routes/listing.js
+++ routes/listing.js
@@ -5,7 +5,8 @@ const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const { storage: cloudinaryStorage } = require("../cloudConfig.js");
+const upload = multer({ storage: cloudinaryStorage });
 
 // create and index route
 router
